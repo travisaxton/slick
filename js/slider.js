5619773abfce854c9a2fb218e694f00d84fe0dc0
Travis Axton <axton@MacBook-Pro.local>
2017-06-11 23:33:12 -0400

    Initial Commit

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slick-container').slick({
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    pauseonFocus: true,
+    pauseonHover: true,
+    speed: 500
+  });
+});
+	
