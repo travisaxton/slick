5cf0e4f21a39da45e4b947e46fcbca52c44d3d87
Chris Rockwell <chris@chrisrockwell.com>
2017-06-14 13:37:59 -0400

    positioning mainly

--- js/slider.js
+++ js/slider.js
@@ -8,7 +8,7 @@ $(document).ready(function($){
   });
 
   $('.slider__nav-thumbnails').slick({
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: '.slider',
     dots: true,
