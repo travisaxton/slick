8ada65737542c8ed7fa126e0202755bea8f0f672
Chris Rockwell <chris@chrisrockwell.com>
2017-06-14 13:19:20 -0400

    Adapt current code to take advantage of navigation example

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,30 @@
-$(document).ready(function(){
-  $('.slick-container').slick({
-    autoplay: true,
-    arrows: true,
-    autoplaySpeed: 5000,
-    fade: true,
-    pauseonFocus: true,
-    pauseonHover: true,
-    speed: 500
+$(document).ready(function($){
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    asNavFor: '.slider-nav-thumbnails',
   });
-});
-	
+
+  $('.slider-nav-thumbnails').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider',
+    dots: true,
+    focusOnSelect: true
+  });
+
+  //remove active class from all thumbnail slides
+  $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+
+  //set active class to first thumbnail slides
+  $('.slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
+
+  // On before slide change match active thumbnail to current slide
+  $('.slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    var mySlideNumber = nextSlide;
+    $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+    $('.slider-nav-thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
+  });
+})(jQuery);
